5abb8fdf2bbb7f0efa95bb095110246432d90faf
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-08-24 21:48:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-// src/App.jsx
-
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareTwitter } from "react-icons/fa6";
 import { AppKitProvider } from './wagmiConfig';
 import ConnectButton from './components/ConnectButton';
+import { FaHeart } from "react-icons/fa";
 
 function App() {
   return (
@@ -14,6 +16,28 @@ function App() {
       <div className=' flex justify-center'>
       <ConnectButton />
       </div>
+        </div>
+
+        <div className=" flex justify-center gap-6 items-center my-6">
+
+          <a href="/" className=" text-4xl text-white">
+          <FaGithub />
+          </a>
+
+          <a href="/" className=" text-4xl text-white">
+          <FaLinkedin />
+          </a>
+
+          <a href="/" className=" text-4xl text-white">
+          <FaSquareTwitter />
+          </a>
+
+        </div>
+
+        <div className=" text-center text-[#acacac] text-base flex justify-center items-center gap-2">
+
+          Made by <a href="/ " className="flex justify-center items-center gap-2">NOORSHANU <FaHeart  className=" text-red-700 "/>
+          </a>
         </div>
       </div>
     </AppKitProvider>
